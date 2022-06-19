fc85239283fa989060598ffa11880165394a3f23
nmausbach <56225438+nmausbach@users.noreply.github.com>
2022-06-19 12:39:11 -0500

    Add files via upload

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,2 +1,6 @@
-pip install pymongo
-pip install pymongo[srv]
+from pymongo import MongoClient
+import pymongo
+url ="mongodb+srv://admin:admin@cluster0.n088j.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(url)
+db = client.pytech
+print(db.list_collection_names())
