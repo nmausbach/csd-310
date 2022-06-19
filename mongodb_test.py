eb96b52cedb4cea73a88e57800ebe6fdc4725bf6
nmausbach <56225438+nmausbach@users.noreply.github.com>
2022-06-19 12:39:47 -0500

    Delete mongodb_test.py

--- mongodb_test.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-url ="mongodb+srv://admin:admin@cluster0.n088j.mongodb.net/?retryWrites=true&w=majority"
-client = MongoClient(url)
-db = client.pytech
-print(db.list_collection_names())
