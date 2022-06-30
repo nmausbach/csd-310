b8db16e550fdd10440393d6d792fa29d6f31fabb
nmausbach <56225438+nmausbach@users.noreply.github.com>
2022-06-29 19:07:14 -0500

    Add files via upload

--- module_6/pytech_delete.py
+++ module_6/pytech_delete.py
@@ -9,15 +9,35 @@ docs = db.students.find({})
 
 #find method
 def findStudent():
-    print("\n--DISPLAYING STUDENTS DOCUMENTS--")
+    print("\n--DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY--")
     for doc in docs:
         print("Student ID: " + doc["student_id"] + "\nFirst Name: " + doc["first_name"] + "\nLast Name: " + doc["last_name"] + "\n")
 
-foo = {"student_id": "1010",
-        "first_name": "Foo",
-        "last_name": "Fighter"}
-#foo_student_id = students.insert_one(foo).inserted_id
+def insertStudent():
+    foo = {"student_id": "1010",
+            "first_name": "Foo",
+            "last_name": "Fighter"}
+    foo_student_id = students.insert_one(foo).inserted_id
+    print("--INSERT STATEMENTS--")
+    print("Inserted student record 1010 into the students collection with document_id")
+    print(foo_student_id)
+    
 
-query = {"student_id": "1010"}
-students.delete_one(query)
+def findUpdatedStudent():
+    print("\n--DISPLAYING NEW STUDENT LIST DOC--")
+    for doc in docs:
+        print("Student ID: " + doc["student_id"] + "\nFirst Name: " + doc["first_name"] + "\nLast Name: " + doc["last_name"] + "\n")
+
+def deleteStudent():
+    query = {"student_id": "1010"}
+    students.delete_one(query)
+    print("--DELETED STUDENT ID: 1010--")
+    for doc in docs:
+        print("Student ID: " + doc["student_id"] + "\nFirst Name: " + doc["first_name"] + "\nLast Name: " + doc["last_name"] + "\n")
+
+
 findStudent()
+insertStudent()
+findUpdatedStudent()
+deleteStudent()
+input("End of program, press any key to continue...")
